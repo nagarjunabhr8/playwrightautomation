277e78857291dacf5f1736de36a3dcaba8b08561
Nagarjuna <nagarjunabhr8@gmail.com>
2026-04-05 20:49:39 +0530

    Remove example spec file

--- tests/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-import fs from 'fs';
-
-test('example test', async ({}, testInfo) => {
-  const file = testInfo.outputPath('temporary-file.txt');
-  await fs.promises.writeFile(file, 'Put some data to the file', 'utf8');
-});
